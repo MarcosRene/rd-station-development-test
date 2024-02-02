c1bc0c02bd21363193128b3aade0ec85e8672e2d
Marcos Renê <renemoura789@gmail.com>
2024-02-02 17:24:05 -0300

    feat: add initial config (colors, sizes, height) tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,66 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1441px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        body: '"Nunito Sans", sans-serif',
+        heading: '"Darker Grotesque", sans-serif',
+      },
+
+      fontSize: {
+        sm: '14px',
+        base: '16px',
+        xl: '20px',
+        '2xl': '24px',
+        '3xl': '32px',
+        '4xl': '40px',
+        '5xl': '56px',
+      },
+
+      lineHeight: {
+        none: '100%',
+        tight: '110%',
+        snug: '120%',
+        relaxed: '140%',
+        loose: '150%',
+      },
+
+      colors: {
+        brand: {
+          300: 'var(--primary-shade-3)',
+          400: 'var(--primary-shade-4)',
+          900: 'var(--primary-shade-1)',
+        },
+
+        highlight: {
+          0: 'var(--hightlight)',
+          50: 'var(--hightlight-50)',
+        },
+
+        bw: {
+          100: 'var(--gray-10)',
+          300: 'var(--gray-30)',
+          600: 'var(--gray-60)',
+          700: 'var(--gray-70)',
+          900: 'var(--gray-100)',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
