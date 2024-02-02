9c9f92a63c2ede55b67b0473cba07f1997344ad5
Marcos Renê <renemoura789@gmail.com>
2024-02-02 18:07:15 -0300

    feat: add layout default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Image from 'next/image'
 
 import '@/app/ui/globals.css'
 
@@ -17,9 +18,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header></header>
-        <main>{children}</main>
-        <footer></footer>
+        <header className="p-6 bg-white flex items-center justify-between md:py-6 md:px-20">
+          <Image
+            src="/logo-rd-station-default.svg"
+            alt="Quatro retângulos 3D cor preta e roxa; Seguido de um texto: Resultados digitais em caixa alta e cor preta"
+            width={100}
+            height={24}
+            className="md:w-[147px] md:h-[36px]"
+          />
+          <h4 className="text-bw-900 text-lg leading-tight font-bold md:text-2xl">
+            Gerador de Cartão de Visita
+          </h4>
+        </header>
+        <main className="h-full py-[54px] px-6 md:py-[72px] md:px-20">
+          {children}
+        </main>
+        <footer className="px-5 py-4 bg-bw-100 flex justify-center gap-4 md:py-6 md:px-20">
+          <span className="underline text-sm font-bold text-bw-900">
+            Política de Privacidade
+          </span>
+          <span className="text-bw-900 text-sm">
+            © 2023 Resultados Digitais
+          </span>
+        </footer>
       </body>
     </html>
   )
