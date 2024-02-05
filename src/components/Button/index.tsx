acc9826cd022e3a4afd7a189cb17b4c73a300d2d
Marcos Renê <renemoura789@gmail.com>
2024-02-04 21:27:54 -0300

    styles: add space bottom when md size

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,7 +12,7 @@ export function Button({ children, icon, rtl = false, ...props }: ButtonProps) {
     <button
       className={`button ${
         rtl ? 'flex-row-reverse' : 'flex-row'
-      } before:clip-path before:absolute before:-bottom-[4px] before:right-0 before:left-0 before:h-1 before:bg-black before:disabled:bg-bw-600 md:-bottom-[5px]`}
+      } before:clip-path before:absolute before:-bottom-[4px] before:right-0 before:left-0 before:h-1 before:bg-black before:disabled:bg-bw-600 md:before:disabled:-bottom-[4px] md:before:-bottom-[5px]`}
       {...props}
     >
       {children}
